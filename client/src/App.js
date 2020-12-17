fa86f3270bd9896c1c352f194304f7324d8d4c50
Alfonso <alfondalix@gmail.com>
2020-12-17 16:15:03 -0300

    CAL-022: Create React App.

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <>
+        <h1>Hello group 7</h1>
+      </>
+    );
+  }
+}
+
+export default App;
