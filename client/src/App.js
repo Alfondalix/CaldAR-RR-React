97fec59577e8b6d0cb5165833aa2a814f5ec1159
Godoy Godoy <alejandogodoy22@gmail.com>
2020-12-22 18:56:28 -0300

    CAL-027: add App first look

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ import Header from './components/layout/header/Header';
 import Sidenav from './components/layout/sidenav/Sidenav';
 import Buildings from './components/pages/Buildings';
 import Companies from './components/pages/Companies';
-import Boilers from './components/pages/Boilers';
+import Boilers from './components/pages/boilers/Boilers.jsx';
 import Technicians from './components/pages/Technicians';
 
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
