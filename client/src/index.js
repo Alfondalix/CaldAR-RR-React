6c230bd74bbabd73837be13e3d6aab6e7d20f790
Alfonso <alfondalix@gmail.com>
2020-12-28 13:12:26 -0300

    CAL-033: import and apply thunk middleware

--- client/src/index.js
+++ client/src/index.js
@@ -3,12 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { composeWithDevTools } from 'redux-devtools-extension';
-import { createStore } from 'redux';
+import { applyMiddleware, createStore } from 'redux';
 import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
 import rootReducer from './redux/reducers/rootReducer';
 
 const configureStore = () => {
-  const enhancer = composeWithDevTools();
+  const enhancer = composeWithDevTools(applyMiddleware(thunk));
   return createStore(rootReducer, enhancer);
 };
 
