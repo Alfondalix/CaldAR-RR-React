a454ea5c0ab9508995f2ac2b1cc5d11fe54ebb11
Godoy Godoy <alejandogodoy22@gmail.com>
2020-12-29 16:55:42 -0300

    CAL-036: Change the root

--- client/src/redux/reducers/rootReducer.js
+++ client/src/redux/reducers/rootReducer.js
@@ -1,13 +1,16 @@
 import { combineReducers } from 'redux';
-
 import CompanyReducer from './companiesReducer';
 import buildingsReducer from './buildingsReducer';
 import BoilerTypesReducer from './boilerTypesReducer';
+import boilersReducer from './Boilers.reducer';
+ 
 
 const rootReducer = combineReducers({
   Company: CompanyReducer,
   buildings: buildingsReducer,
   BoilerTypes: BoilerTypesReducer,
+  boilers: boilersReducer,
+
 });
 
 export default rootReducer;
