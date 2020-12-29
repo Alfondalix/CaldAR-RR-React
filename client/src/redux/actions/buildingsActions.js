3a0ce0eecbbdd7c2e968d2bc44ca7c6d6e4a9e9f
Alfonso <alfondalix@gmail.com>
2020-12-29 00:25:01 -0300

    CAL-034: Starting with reducers and actions

---
+++ client/src/redux/actions/buildingsActions.js
@@ -0,0 +1,8 @@
+const SHOW_BUILDINGS = 'SHOW_BUILDINGS';
+const ADD_BUILDING = 'ADD_BUILDING';
+const EDIT_BUILDING = 'EDIT_BUILDING';
+const DELETE_BUILDING = 'DELETE_BUILDING';
+
+export const showBuildings = async dispatch => {
+  const response = await
+}
