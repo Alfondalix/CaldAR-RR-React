cef790de3039697ddad38129524bcd53f719cf4f
Godoy Godoy <alejandogodoy22@gmail.com>
2020-12-29 16:57:43 -0300

    CAL-036: Add Boilers reducer, first look

---
+++ client/src/redux/reducers/Boilers.reducer.js
@@ -0,0 +1,28 @@
+import { ADD_BOILER, DELETE_BOILER, EDIT_BOILER } from '../types/boilerTypes';
+import boilers from '../../pages/boilers/Boilers.json';
+
+const boilersReducer = (state = boilers, action) => {
+  switch (action.type) {
+    case ADD_BOILER: {
+      return [...state, action.payload];
+    }
+    case DELETE_BOILER: {
+      return [...state.filter((boiler) => boiler.id !== action.payload)];
+    }
+    case EDIT_BOILER: {
+      return [
+        ...state.map((boiler) => {
+          if (boiler.id === action.payload.id) {
+            boiler = action.payload;
+          }
+          return boiler;
+        }),
+      ];
+    }
+    default: {
+      return state;
+    }
+  }
+};
+
+export default boilersReducer;
