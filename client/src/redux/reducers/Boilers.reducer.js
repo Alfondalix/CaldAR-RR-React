16cca52f31f310db6c9086df45ca69abd2496715
Godoy Godoy <alejandogodoy22@gmail.com>
2020-12-29 23:15:38 -0300

    CAL-036: Update Boiler reducer

--- client/src/redux/reducers/Boilers.reducer.js
+++ client/src/redux/reducers/Boilers.reducer.js
@@ -1,27 +1,80 @@
-import { ADD_BOILER, DELETE_BOILER, EDIT_BOILER } from '../types/boilerTypes';
-import boilers from '../../pages/boilers/Boilers.json';
+import {
+  GET_BOILERS_PENDING,
+  GET_BOILERS_FULFILLED,
+  GET_BOILERS_REJECTED,
+  POST_BOILERS_PENDING,
+  POST_BOILERS_FULFILLED,
+  POST_BOILERS_REJECTED,
+  PUT_BOILERS_PENDING,
+  PUT_BOILERS_FULFILLED,
+  PUT_BOILERS_REJECTED,
+  DELETE_BOILERS_PENDING,
+  DELETE_BOILERS_FULFILLED,
+  DELETE_BOILERS_REJECTED,
+} from '../types/Boiler.types';
 
-const boilersReducer = (state = boilers, action) => {
+const initialState = {
+  list: [],
+};
+
+const boilersReducer = (state = initialState, action) => {
   switch (action.type) {
-    case ADD_BOILER: {
-      return [...state, action.payload];
-    }
-    case DELETE_BOILER: {
-      return [...state.filter((boiler) => boiler.id !== action.payload)];
-    }
-    case EDIT_BOILER: {
-      return [
-        ...state.map((boiler) => {
-          if (boiler.id === action.payload.id) {
-            boiler = action.payload;
-          }
-          return boiler;
-        }),
-      ];
-    }
-    default: {
+    case GET_BOILERS_PENDING:
+      return {
+        ...state,
+      };
+    case GET_BOILERS_FULFILLED:
+      return {
+        ...state,
+        list: action.payload,
+      };
+    case GET_BOILERS_REJECTED:
+      return {
+        ...state,
+      };
+    case POST_BOILERS_PENDING:
+      return {
+        ...state,
+      };
+    case POST_BOILERS_FULFILLED:
+      return {
+        ...state,
+        list: [...state.list, action.payload],
+      };
+    case POST_BOILERS_REJECTED:
+      return {
+        ...state,
+      };
+    case DELETE_BOILERS_PENDING:
+      return {
+        ...state,
+      };
+    case DELETE_BOILERS_FULFILLED:
+      return {
+        ...state,
+        list: state.list.filter((boiler) => boiler._id !== action.payload),
+      };
+    case DELETE_BOILERS_REJECTED:
+      return {
+        ...state,
+      };
+    case PUT_BOILERS_PENDING:
+      return {
+        ...state,
+      };
+    case PUT_BOILERS_FULFILLED:
+      return {
+        ...state,
+        list: state.list.map((boiler) =>
+          boiler._id === action.payload ? (boiler = action.payload) : boiler
+        ),
+      };
+    case PUT_BOILERS_REJECTED:
+      return {
+        ...state,
+      };
+    default:
       return state;
-    }
   }
 };
 
