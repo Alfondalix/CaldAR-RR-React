61d78cc0db6ba4e7ea6c7bf1a8929480e7f946d0
Tomas Cravero <tomascravero25@gmail.com>
2020-12-30 01:19:46 -0300

    CAL-035: edit reducer & actions

--- client/src/redux/actions/companiesActions.js
+++ client/src/redux/actions/companiesActions.js
@@ -1,8 +1,21 @@
-import { SHOW_COMPANIES_FETCHING, SHOW_COMPANIES_REJECTED, SHOW_COMPANIES_FULFILLED } from '../types/companiesTypes';
+import {
+  SHOW_COMPANIES_FETCHING,
+  SHOW_COMPANIES_REJECTED,
+  SHOW_COMPANIES_FULFILLED,
+  ADD_COMPANY_FETCHING,
+  ADD_COMPANY_FULFILLED,
+  ADD_COMPANY_REJECTED,
+  EDIT_COMPANY_FETCHING,
+  EDIT_COMPANY_FULFILLED,
+  EDIT_COMPANY_REJECTED,
+  DELETE_COMPANY_FETCHING,
+  DELETE_COMPANY_FULFILLED,
+  DELETE_COMPANY_REJECTED,
+} from '../types/companiesTypes';
 
-const URL = "https://caldar-rr-alfonso.herokuapp.com/api/companies";
+const URL = 'https://caldar-rr-alfonso.herokuapp.com/api/companies';
 
-const getCompaniesFetching = () => dispatch => ({
+const getCompaniesFetching = () => (dispatch) => ({
   type: SHOW_COMPANIES_FETCHING,
 });
 
@@ -11,19 +24,100 @@ const getCompaniesFulfilled = (payload) => ({
   payload,
 });
 
-const getCompaniesRejected = () => dispatch => ({
+const getCompaniesRejected = () => (dispatch) => ({
   type: SHOW_COMPANIES_REJECTED,
 });
 
-const getCompanies = () => dispatch => {
-
+export const getCompanies = () => (dispatch) => {
   dispatch(getCompaniesFetching());
   return fetch(URL)
-    .then(data => data.json())
-    .then (response => {
+    .then((data) => data.json())
+    .then((response) => {
       dispatch(getCompaniesFulfilled(response));
     })
     .catch((err) => {
       dispatch(getCompaniesRejected(err));
+    });
+};
+
+const addCompanyFetching = () => ({
+  type: ADD_COMPANY_FETCHING,
+});
+
+const addCompanyFulfilled = (payload) => ({
+  type: ADD_COMPANY_FULFILLED,
+  payload,
+});
+
+const addCompanyRejected = () => ({
+  type: ADD_COMPANY_REJECTED,
+});
+
+export const addCompany = (company) => (dispatch) => {
+  dispatch(addCompanyFetching());
+  return fetch(URL, {
+    method: 'POST',
+    body: JSON.stringify(company),
+  })
+    .then((data) => data.json())
+    .then((response) => {
+      dispatch(addCompanyFulfilled(response));
+    })
+    .catch((err) => {
+      dispatch(addCompanyRejected(err));
+    });
+};
+
+const deleteCompanyFetching = () => ({
+  type: DELETE_COMPANY_FETCHING,
+});
+
+const deleteCompanyFulfilled = (payload) => ({
+  type: DELETE_COMPANY_FULFILLED,
+  payload,
+});
+
+const deleteCompanyRejected = () => ({
+  type: DELETE_COMPANY_REJECTED,
+});
+
+export const deleteCompany = (id) => (dispatch) => {
+  dispatch(deleteCompanyFetching());
+  return fetch(`${URL}${resource}/${_id}`, { method: 'DELETE' })
+    .then((data) => data.json())
+    .then(() => {
+      dispatch(deleteCompanyFulfilled(id));
+    })
+    .catch(() => {
+      dispatch(deleteCompanyRejected());
+    });
+};
+
+const updateCompanyFetching = () => ({
+  type: EDIT_COMPANY_FETCHING,
+});
+
+const updateCompanyFulfilled = (payload) => ({
+  type: EDIT_COMPANY_FULFILLED,
+  payload,
+});
+
+const updateCompanyRejected = () => ({
+  type: EDIT_COMPANY_REJECTED,
+});
+
+export const updateCompany = (newCompany) => {
+
+  dispatch(updateCompanyFetching());
+  return fetch(`${URL}/${newCompany._id}`, {
+    method: 'PUT',
+    body: JSON.stringify({ newCompany }),
+  })
+    .then((response) => response.json())
+    .then(() => {
+      dispatch(updateCompanyFulfilled(newCompany));
+    })
+    .catch((error) => {
+      dispatch(updateCompanyRejected(error));
     });
 };
--- client/src/redux/reducers/companiesReducer.js
+++ client/src/redux/reducers/companiesReducer.js
@@ -8,8 +8,8 @@ import {
   EDIT_COMPANY_FETCHING,
   EDIT_COMPANY_FULFILLED,
   EDIT_COMPANY_REJECTED,
-  DELETE_COMPANY_FETCHIN,
-  DELETE_COMPANY_FULFILLE,
+  DELETE_COMPANY_FETCHING,
+  DELETE_COMPANY_FULFILLED,
   DELETE_COMPANY_REJECTED,
 } from '../types/companiesTypes';
 
@@ -32,7 +32,45 @@ const CompanyReducer = (state = initialState, action) => {
       return {
         ...state,
       };
-
+    case ADD_COMPANY_FETCHING:
+      return {
+        ...state,
+      };
+    case ADD_COMPANY_FULFILLED:
+      return {
+        ...state,
+        list: action.payload,
+      };
+    case ADD_COMPANY_REJECTED:
+      return {
+        ...state,
+      };
+    case DELETE_COMPANY_FETCHING:
+      return {
+        ...state,
+      };
+    case DELETE_COMPANY_FULFILLED:
+      return {
+        ...state,
+        list: state.list.filter((company) => company._id !== action.payload),
+      };
+    case DELETE_COMPANY_REJECTED:
+      return {
+        ...state,
+      };
+    case EDIT_COMPANY_FETCHING:
+      return {
+        ...state,
+      };
+    case EDIT_COMPANY_FULFILLED:
+      return {
+        ...state,
+        list: state.list.map((company) => company._id === action.payload ? (company = action.payload) : company),
+      };
+    case EDIT_COMPANY_REJECTED:
+      return {
+        ...state,
+      };
     default:
       return state;
   }
