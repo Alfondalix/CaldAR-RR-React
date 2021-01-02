38e2cbe26645a3ce62177e4b6b5d3c316bcb7a77
Alfonso <alfondalix@gmail.com>
2021-01-02 12:42:41 -0300

    CAL-034: adding useEffect and mapstate

--- client/src/components/pages/buildings/BuildingTable.jsx
+++ client/src/components/pages/buildings/BuildingTable.jsx
@@ -1,6 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+
+import {
+  getBuildings,
+  postBuilding,
+  deleteBuilding,
+  putBuilding,
+} from '../../../redux/actions/buildingsActions';
 
 const BuildingTable = (props) => {
+  useEffect(() => {
+    getBuildings();
+  }, []);
+
   return (
     <div className="table-container">
       <table className="buildings-table">
@@ -15,12 +28,12 @@ const BuildingTable = (props) => {
           </tr>
         </thead>
         <tbody classname="table-body">
-          {props.building.map((building) => (
+          {props.buildings.list.map((building) => (
             <tr className="table-row" key={building.id}>
               <td>{building.id}</td>
               <td>{building.fullName}</td>
               <td>{building.address}</td>
-              <td>{building.boilers}</td>
+              <td>[{building.boilers.join('-')}]</td>
               <td>{building.phoneNumber}</td>
               <td>
                 <button
@@ -45,4 +58,19 @@ const BuildingTable = (props) => {
   );
 };
 
-export default BuildingTable;
+const mapDispatchToProps = (dispatch) =>
+  bindActionCreators(
+    {
+      getBuildings,
+      deleteBuilding,
+      postBuilding,
+      putBuilding,
+    },
+    dispatch
+  );
+
+const mapStateToProps = (state) => ({
+  buildings: state.buildings,
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(BuildingTable);
