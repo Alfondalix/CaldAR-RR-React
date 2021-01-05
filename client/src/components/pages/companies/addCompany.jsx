2e40c696351568100e7139a2bc7d54f92a39761f
Tomas Cravero <tomascravero25@gmail.com>
2021-01-05 18:08:23 -0300

    CAL-043: final form to addcompanies

--- client/src/components/pages/companies/addCompany.jsx
+++ client/src/components/pages/companies/addCompany.jsx
@@ -6,6 +6,7 @@ import CancelIcon from '@material-ui/icons/Cancel';
 import TextField from '@material-ui/core/TextField';
 import Modal from '@material-ui/core/Modal';
 import styles from './forms.module.css';
+// import { render } from 'react-dom';
 import { Form, Field } from 'react-final-form';
 
 const AddCompany = (props) => {
@@ -26,7 +27,7 @@ const AddCompany = (props) => {
   };
 
   const handleSubmit = (e) => {
-    e.preventDefault();
+    // e.preventDefault();
     props.addCompany(company);
     setOpen(false);
     setCompany(newCompany);
@@ -40,54 +41,89 @@ const AddCompany = (props) => {
     setOpen(false);
   };
 
+  const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
+  const onSubmit = async (values, e) => {
+    await sleep(300);
+    handleSubmit();
+  };
+
+  const required = (value) => (value ? undefined : 'Required');
+  // const addressValidation = (value) => ()
+
   return (
     <div className={styles.container}>
       <Button variant="contained" color="primary" onClick={handleOpen}>
         <AddBoxIcon />
       </Button>
       <Modal open={open} onClose={handleClose}>
-        <form className={styles.formModal}>
-          <TextField
-            id="outlined-basic"
-            label="CUIT"
-            variant="outlined"
-            onChange={handleChange}
-            value={company.cuit}
-            name="cuit"
-          />
-          <TextField
-            id="outlined-basic"
-            label="EMAIL"
-            variant="outlined"
-            onChange={handleChange}
-            value={company.email}
-            name="email"
-          />
-          <TextField
-            id="outlined-basic"
-            label="ADDRESS"
-            variant="outlined"
-            onChange={handleChange}
-            value={company.adress}
-            name="adress"
-          />
-          <TextField
-            id="outlined-basic"
-            label="BUILDINGS"
-            variant="outlined"
-            onChange={handleChange}
-            value={company.buildings}
-            name="buildings"
-          />
-          <div>
-            <Button onClick={handleSubmit}>
-              <CheckCircleIcon />
-            </Button>
-            <Button onClick={handleClose}>
-              <CancelIcon />
-            </Button>
-          </div>
-        </form>
+        <Form
+        onSubmit={onSubmit}
+          render={({ handleSubmit, form, submitting, pristine, values }) => (
+            <form className={styles.formModal} onChange={handleChange}>
+              <Field name="cuit" validate={required} value={company.cuit}>
+                {({ input, meta }) => (
+                  <div>
+                    <TextField
+                      id="outlined-basic"
+                      label="CUIT"
+                      variant="outlined"
+                      {...input}
+                      // name="cuit"
+                    />
+                    {meta.error && meta.touched && <span className={styles.error}>{meta.error}</span>}
+                  </div>
+                )}
+              </Field>
+              <Field name="email" validate={required}>
+                {({ input, meta }) => (
+                  <div>
+                    <TextField
+                      id="outlined-basic"
+                      label="EMAIL"
+                      variant="outlined"
+                      value={company.email}
+                      {...input}
+                      name="email"
+                    />
+                    {meta.error && meta.touched && <span className={styles.error}>{meta.error}</span>}
+                  </div>
+                )}
+              </Field>
+              <Field name="adress" validate={required}>
+                {({ input, meta }) => (
+                  <div>
+                    <TextField
+                      id="outlined-basic"
+                      label="ADDRESS"
+                      variant="outlined"
+                      onChange={handleChange}
+                      value={company.adress}
+                      {...input}
+                      name="adress"
+                    />
+                    {meta.error && meta.touched && <span className={styles.error}>{meta.error}</span>}
+                  </div>
+                )}
+              </Field>
+              <TextField
+                id="outlined-basic"
+                label="BUILDINGS"
+                variant="outlined"
+                value={company.buildings}
+                name="buildings"
+              />
+              <div>
+                <Button onClick={handleSubmit} disabled={submitting}>
+                  <CheckCircleIcon />
+                </Button>
+                <Button onClick={handleClose}>
+                  <CancelIcon />
+                </Button>
+              </div>
+            </form>
+          )}
+        />
       </Modal>
     </div>
   );
