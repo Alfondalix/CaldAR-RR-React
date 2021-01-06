b070629adb1f19fad5c71f0fdf52e2e7db835ddd
Godoy Godoy <alejandogodoy22@gmail.com>
2021-01-06 02:32:20 -0300

    CAL-045: validation, change idType input. First look

--- client/src/components/pages/boilers/AddBoiler.jsx
+++ client/src/components/pages/boilers/AddBoiler.jsx
@@ -20,8 +20,9 @@ const AddBoiler = (props) => {
   };
 
   const handleSubmit = (e) => {
-    e.preventDefault();
+    // e.preventDefault();
     props.postBoiler(boiler);
+    setBoiler(newBoiler);
     setOpen(false);
   };
 
@@ -33,6 +34,15 @@ const AddBoiler = (props) => {
     setOpen(false);
   };
 
+  const required = (value) => (value ? undefined : 'Required *');
+
+  const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
+  const onSubmit = async (values, e) => {
+    await sleep(300);
+    handleSubmit();
+  };
+
   return (
     <>
       <button className="add-btn" type="button" onClick={handleOpen}>
@@ -41,51 +51,82 @@ const AddBoiler = (props) => {
       <div>
         <Modal
           open={open}
-          onClose={(handleSubmit, handleClose)}
+          onClose={handleClose}
           aria-labelledby="Add Boiler"
           className="modal"
         >
-          <form action="Submit" className="edit-form">
-            <input
-              className="u-full-width"
-              type="text"
-              name="idType"
-              placeholder="please, enter ID type"
-              value={boiler.idType}
-              onChange={handleChange}
-            />
-            <input
-              className="u-full-width"
-              type="time"
-              name="startTime"
-              placeholder="enter initial hour"
-              value={boiler.startTime}
-              onChange={handleChange}
-            />
-            <input
-              className="u-full-width"
-              type="time"
-              name="endTime"
-              placeholder="Enter finish hour"
-              value={boiler.endTime}
-              onChange={handleChange}
-            />
-            <input
-              className="u-full-width"
-              type="number"
-              name="monthlyHours"
-              placeholder="Monthly Hours"
-              value={boiler.monthlyHours}
-              onChange={handleChange}
-            />
-            <button
-              className="button-primary"
-              type="submit"
-              onClick={handleSubmit}
-            >
-              Add new Boiler
-            </button>
-          </form>
+          <Form
+            onSubmit={onSubmit}
+            render={({ handleSubmit, values, submitting }) => (
+              <form className="edit-form" onChange={handleChange}>
+                <label>Boiler ID Type</label>
+                <Field name="idType" value={boiler.idType} component="select">
+                  <option value=""></option>
+                  <option value="A">A</option>
+                  <option value="B">B</option>
+                  <option value="C">C</option>
+                  <option value="D">D</option>
+                </Field>
+                <label>Start Time</label>
+                <Field
+                  name="startTime"
+                  value={boiler.startTime}
+                  validate={required}
+                >
+                  {({ input, meta }) => (
+                    <div className="input-container">
+                      <input {...input} className="u-full-width" type="time" />
+                      {meta.error && meta.touched && (
+                        <span className="error-input">{meta.error}</span>
+                      )}
+                    </div>
+                  )}
+                </Field>
+                <label>End Time</label>
+                <Field
+                  name="endTime"
+                  value={boiler.endTime}
+                  validate={required}
+                >
+                  {({ input, meta }) => (
+                    <div className="input-container">
+                      <input {...input} className="u-full-width" type="time" />
+                      {meta.error && meta.touched && (
+                        <span className="error-input">{meta.error}</span>
+                      )}
+                    </div>
+                  )}
+                </Field>
+                <label>Total amount of Hours</label>
+                <Field
+                  name="monthlyHours"
+                  value={boiler.monthlyHours}
+                  validate={required}
+                >
+                  {({ input, meta }) => (
+                    <div className="input-container">
+                      <input
+                        {...input}
+                        className="u-full-width"
+                        type="number"
+                      />
+                      {meta.error && meta.touched && (
+                        <span className="error-input">{meta.error}</span>
+                      )}
+                    </div>
+                  )}
+                </Field>
+                <button
+                  className="button-primary"
+                  type="submit"
+                  onClick={handleSubmit}
+                  disabled={submitting}
+                >
+                  Add Boiler
+                </button>
+              </form>
+            )}
+          />
         </Modal>
       </div>
     </>
