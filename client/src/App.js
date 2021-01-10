6a15a0b5e70eaf043e823c8077c8ba60dd64d1d9
Tomas Cravero <tomascravero25@gmail.com>
2021-01-10 17:03:49 -0300

    CAL-052: log in component

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Header from './components/layout/header/Header';
 import Sidenav from './components/layout/sidenav/Sidenav';
@@ -8,23 +8,32 @@ import Boilers from './components/pages/boilers/Boilers.jsx';
 import Technicians from './components/pages/Technicians';
 import BoilerTypes from './components/pages/Boiler-types/BoilerTypes.jsx';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import LogIn from './components/pages/log-in/logIn';
 
 const App = () => {
-  return (
-    <Router>
-      <div className="App">
-        <Header />
-        <Sidenav />
-        <Switch>
-          <Route path="/companies" exact component={Companies} />
-          <Route path="/buildings" exact component={Buildings} />
-          <Route path="/boilers" exact component={Boilers} />
-          <Route path="/technicians" exact component={Technicians} />
-          <Route path="/boiler-types" exact component={BoilerTypes} />
-        </Switch>
-      </div>
-    </Router>
-  );
+  const [log, setLog] = useState(true);
+  
+  if (log) {
+    return (
+      <Router>
+        <div className="App">
+          <Header />
+          <Sidenav />
+          <Switch>
+            <Route path="/companies" exact component={Companies} />
+            <Route path="/buildings" exact component={Buildings} />
+            <Route path="/boilers" exact component={Boilers} />
+            <Route path="/technicians" exact component={Technicians} />
+            <Route path="/boiler-types" exact component={BoilerTypes} />
+          </Switch>
+        </div>
+      </Router>
+    );
+  } else {
+    return (
+      <LogIn />
+    );
+  }
 };
 
 export default App;
